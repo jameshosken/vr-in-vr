9310478a865d8abebaf80a3d5076f8d90bb2afc0
James Hosken <jameswfhosken@gmail.com>
2020-01-26 13:49:40 -0500

    Commit from stupid hackathon
    Oculus library ignored, be sure to install it before running project

---
+++ Virtual Reality in VR/Assets/CalibrationController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CalibrationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform calibrationCenter;
+    public Transform leftController;
+    public Transform rightController;
+    public Transform playerController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool button_B = OVRInput.Get(OVRInput.RawButton.B);
+        bool button_Y = OVRInput.Get(OVRInput.RawButton.Y);
+
+        if(button_B && button_Y)
+        {
+            //Calibrate();
+        }
+    }
+
+    //void Calibrate()
+    // TODO fix
+    //{
+    //    Debug.Log(calibrationCenter.position);
+    //    Vector3 leftHandLocalOffset = (leftController.localPosition + Vector3.up*2)*-1;
+
+    //    calibrationCenter.position = leftController.position;
+
+    //    Debug.Log(calibrationCenter.position);
+
+    //    playerController.localPosition = leftHandLocalOffset;
+    //    playerController.localRotation = Quaternion.identity;
+
+    //    Debug.Log("...................");
+    //    //Debug.Log("...................");
+
+    //}
+}
---
+++ Virtual Reality in VR/Assets/HMDSwitcherController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HMDSwitcherController : MonoBehaviour
+{
+    public Collider headsetCollider;
+    Collider headCollider;
+
+    public Transform HMDHeadpoint;
+
+    bool isLocked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        headCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (isLocked)
+        {
+            LockHeadset();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        bool leftRelease = OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger) && !OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
+        bool rightRelease = OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger) && !OVRInput.Get(OVRInput.Button.PrimaryHandTrigger);
+
+
+        bool leftGrab = OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger) ;
+        bool rightGrab = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger);
+
+        if (rightRelease || leftRelease)
+        {
+            isLocked = true;
+        }
+
+        if (leftGrab || rightGrab)
+        {
+            isLocked = false;
+        }
+
+
+    }
+
+    private void LockHeadset()
+    {
+        HMDHeadpoint.position = transform.position + transform.forward * 0.025f;
+        HMDHeadpoint.rotation = transform.rotation;
+
+    }
+}
